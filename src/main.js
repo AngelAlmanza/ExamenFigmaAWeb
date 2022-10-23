c16d2205b4efa781dbedb1d6c116a86a90fb6a88
Angel Almanza <almanza.angel245@gmail.com>
2022-10-23 11:40:38 -0600

    Menu hamburguesa mobile y tablet creados y funcionando, pendiente modificar el menu para versiones desktop

--- src/main.js
+++ src/main.js
@@ -1,25 +1,37 @@
 const menuHamburguesa = document.querySelector('.menu-hamburguesa');
 const menuHeader = document.querySelector('.menu-header');
+const lineaUno = document.querySelector('.menu-hamburguesa__linea1');
+const lineaDos = document.querySelector('.menu-hamburguesa__linea2');
+const lineaTres = document.querySelector('.menu-hamburguesa__linea3');
 
-if (window.innerWidth >= 1024) {
-  menuHamburguesa.classList.add('inactive');
-  menuHeader.classList.remove('inactive');
-} else {
-  menuHamburguesa.classList.remove('inactive');
-  menuHeader.classList.add('inactive');
+const menuAnimado = () => {
+  lineaUno.classList.toggle('activemenu-hamburguesa__linea1');
+  lineaDos.classList.toggle('activemenu-hamburguesa__linea2');
+  lineaTres.classList.toggle('activemenu-hamburguesa__linea3');
+  menuHeader.classList.toggle('menu-headeractive');
 }
 
-window.onresize = () => {
-  if (window.innerWidth >= 1024 && !menuHamburguesa.classList.contains('inactive')) {
-    menuHamburguesa.classList.add('inactive');
-  }
-  if (window.innerWidth < 1024 && menuHamburguesa.classList.contains('inactive')) {
-    menuHamburguesa.classList.remove('inactive');
-  }
-  if (window.innerWidth >= 1024 && menuHeader.classList.contains('inactive')) {
-    menuHeader.classList.remove('inactive');
-  }
-  if (window.innerWidth < 1024 && !menuHeader.classList.contains('inactive')) {
-    menuHeader.classList.add('inactive');
-  }
-}
+menuHamburguesa.addEventListener('click', menuAnimado);
+
+// if (window.innerWidth >= 1024) {
+//   menuHamburguesa.classList.add('inactive');
+//   menuHeader.classList.remove('inactive');
+// } else {
+//   menuHamburguesa.classList.remove('inactive');
+//   menuHeader.classList.add('inactive');
+// }
+
+// window.onresize = () => {
+//   if (window.innerWidth >= 1024 && !menuHamburguesa.classList.contains('inactive')) {
+//     menuHamburguesa.classList.add('inactive');
+//   }
+//   if (window.innerWidth < 1024 && menuHamburguesa.classList.contains('inactive')) {
+//     menuHamburguesa.classList.remove('inactive');
+//   }
+//   if (window.innerWidth >= 1024 && menuHeader.classList.contains('inactive')) {
+//     menuHeader.classList.remove('inactive');
+//   }
+//   if (window.innerWidth < 1024 && !menuHeader.classList.contains('inactive')) {
+//     menuHeader.classList.add('inactive');
+//   }
+// }
