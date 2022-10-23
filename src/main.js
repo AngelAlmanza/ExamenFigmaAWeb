c94f0218b0316e0b027f27ed962ba41300768bd1
Angel Almanza <almanza.angel245@gmail.com>
2022-10-23 11:55:50 -0600

    Retirado codigo basura de JS

--- src/main.js
+++ src/main.js
@@ -12,26 +12,3 @@ const menuAnimado = () => {
 }
 
 menuHamburguesa.addEventListener('click', menuAnimado);
-
-// if (window.innerWidth >= 1024) {
-//   menuHamburguesa.classList.add('inactive');
-//   menuHeader.classList.remove('inactive');
-// } else {
-//   menuHamburguesa.classList.remove('inactive');
-//   menuHeader.classList.add('inactive');
-// }
-
-// window.onresize = () => {
-//   if (window.innerWidth >= 1024 && !menuHamburguesa.classList.contains('inactive')) {
-//     menuHamburguesa.classList.add('inactive');
-//   }
-//   if (window.innerWidth < 1024 && menuHamburguesa.classList.contains('inactive')) {
-//     menuHamburguesa.classList.remove('inactive');
-//   }
-//   if (window.innerWidth >= 1024 && menuHeader.classList.contains('inactive')) {
-//     menuHeader.classList.remove('inactive');
-//   }
-//   if (window.innerWidth < 1024 && !menuHeader.classList.contains('inactive')) {
-//     menuHeader.classList.add('inactive');
-//   }
-// }
